f4c0dcf92c8e77e3464577927e78434e8041631e
Paddy Wardle <paddy.wardle@yahoo.com>
2023-01-19 16:43:31 +0000

    Mesh.py changes - changing cell_volumes() method to use tetrahedron decomposition and implementing convex_hull method

--- src/Mesh.py
+++ src/Mesh.py
@@ -1,5 +1,7 @@
 from typing import List
 import numpy as np
+from scipy.spatial import ConvexHull
+
 
 class Mesh:
 
@@ -84,6 +86,34 @@ class Mesh:
         Returns:
             np.array: array containing the volume of each cell in the mesh
         """
+        # should I calculate cell and face centre lists using other functions and then use these values in the loop rather than calculating each time?
+        # empty array for cell volumes to be added to
+        cell_vols = np.array([])
+
+        for cell in self.cells:
+            # get unique faces that make up the cell
+            cell_points_num = np.unique(np.concatenate(self.faces[cell]))
+            cell_points = self.points[cell_points_num]
+            cell_centre = np.divide(cell_points.sum(axis=0), len(cell_points))
+            cell_vol = 0
+            for face in cell:
+                # get points that make up the face
+                face_points = self.points[self.faces[face]]
+                face_centre = np.divide(face_points.sum(axis=0), len(face_points))
+                for i in range(len(face_points)):
+                    if i == (len(face_points)-1):
+                        vertices = np.concatenate(([face_points[i]], [face_points[0]], [face_centre], [cell_centre]), axis=0)
+                        coord_matrix = np.column_stack((vertices, np.ones((len(vertices), 1))))
+                        cell_vol += np.linalg.det(coord_matrix) / 6
+                        continue
+                    vertices = np.concatenate(([face_points[i]], [face_points[i+1]], [face_centre], [cell_centre]), axis=0)
+                    coord_matrix = np.column_stack((vertices, np.ones((len(vertices), 1))))
+                    cell_vol += np.linalg.det(coord_matrix) / 6
+            cell_vols = np.append(cell_vols, cell_vol)
+        
+        return cell_vols
+
+    def convex_hull_volume(self):
 
         # empty array for cell volumes to be added to
         cell_vols = np.array([])
@@ -92,16 +122,40 @@ class Mesh:
             # get unique faces that make up the cell
             cell_faces = np.unique(np.concatenate(self.faces[cell]))
             cell_points = self.points[cell_faces]
-            
-            # get min and max x, y and z points for the cell
-            cell_min_points = cell_points.min(axis=0)
-            cell_max_points = cell_points.max(axis=0)
-            
-            # multiply difference between min and max points to get cell volume
-            cell_vol = np.prod(cell_max_points - cell_min_points)
+            cell_vol = ConvexHull(cell_points).volume
             cell_vols = np.append(cell_vols, cell_vol)
-
+        
         return cell_vols
+
+    def face_area_vectors(self):
+
+        """
+        This function returns a numpy array of the face area vector for each face in the mesh instance.
+
+        Returns:
+            np.array: array containing the face area vector of each cell in the mesh
+        """
+
+        # empty array for cell face area vectors
+        face_area_vecs = np.array([])
+
+        for face in self.faces:
+            # get points that make up face
+            face_points = self.points[face]
+
+
+            # get min and max x, y and z points for the face
+            face_min_points = face_points.min(axis=0)
+            face_max_points = face_points.max(axis=0)
+
+            # calculate difference between min and max points
+            point_diff = face_max_points-face_min_points
+
+            # multiply together points that are greater than 0 to get face area
+            face_area = np.prod(point_diff[point_diff > 0])
+            face_area_vecs = np.append(face_area_vecs, face_area)
+
+        return face_area_vecs
 
     def face_area_vectors(self):
 
