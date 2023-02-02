e228918bb0a77c620f2f61b6c37a9bfe223d4f96
Paddy Wardle <paddy.wardle@yahoo.com>
2023-02-02 17:54:21 +0000

    Updates to LinearSystem class

--- src/LinearSystem.py
+++ src/LinearSystem.py
@@ -4,22 +4,63 @@ import numpy as np
 
 class LinearSystem:
 
-    def __init__(self, mesh, Re):
+    def __init__(self, mesh, viscosity):
 
         self.mesh = mesh
-        self.Re = Re
+        self.viscoity = viscosity
 
-    def A_disc(self):
+    def A_disc(self, u, dx):
 
-        N = self.mesh.cells * self.mesh.cells
+        """
+        This function discretises the momentum equation to get the contributions to the linear system
+
+        Returns:
+            np.array: N x N matrix defining contributions of convective and diffusion terms to the linear system.
+
+        """
+        N = len(self.mesh.cells)
 
         A = np.zeros((N, N))
 
-        #for i in self.mesh.cells:
+        for i in range(len(self.mesh.cells)):
+
+            diag_cont = 0
+            neighbours = self.mesh.neighbouring_cells()[i]
+
+            # THIS DOESN'T GET THE CORRECT FACE AREA VECTOR!
+            area_mag = np.linalg.norm(self.mesh.face_area_vectors()[i])
+
+            # ADD LOGIC FOR THE FACE OWNER!
+
+            for j in neighbours:
+
+                off_diag = 0
+
+                # second order postulation for face area velocity
+                u_face = u[i] + dx * abs(u[i] - u[j])
+
+                # convective contribution
+                diag_cont += max(area_mag * u_face, 0)
+                off_diag += min(area_mag * u_face, 0)
+
+                # diffusion contributions
+                diag_cont += -self.viscoity * area_mag / dx
+                off_diag += self.viscoity * area_mag / dx
+
+                A[i, j] = off_diag
+            
+            A[i, i] = diag_cont
+
+        #A_sparse = SparseMatrixCR(N, N).from_dense(A)
+
+        return A
 
     def b_disc(self):
 
-        pass
+        # steady state simulation so will be a vector of zeros
+        N = len(self.mesh.cells)
+
+        return np.zeros((N, 1)) 
 
     def solver(self):
 
