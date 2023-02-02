b32d4f8e587b3a3411bc61d670a217ab3f75d79a
Paddy Wardle <paddy.wardle@yahoo.com>
2023-02-02 17:54:57 +0000

    Updating ReadMesh.py to correctly read in each mesh file

--- src/ReadMesh.py
+++ src/ReadMesh.py
@@ -2,6 +2,7 @@ import numpy as np
 
 def read_mesh(points_filename, faces_filename, cells_filename, boundary_filename):
 
+    # call read_file function for each mesh characteristic and return arrays
     points = np.asarray(read_file(points_filename))
     faces = np.asarray(read_file(faces_filename), dtype=int)
     cells = np.asarray(read_file(cells_filename), dtype=int)
@@ -13,6 +14,7 @@ def read_file(filename):
 
     array = []
 
+    # read each file for the mesh, appropriately format and return array of mesh characteristic
     with open("MeshFiles/" + filename, 'r') as f:
         for line in f.readlines():
             line = line.strip()
@@ -22,5 +24,5 @@ def read_file(filename):
                 ls = [float(i) for i in ls]
             
             array.append(ls)
-            
+
     return array
