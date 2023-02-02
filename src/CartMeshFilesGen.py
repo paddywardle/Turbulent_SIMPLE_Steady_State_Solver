e91d728e92ee34fc6a275c28d3d294906465eadd
Paddy Wardle <paddy.wardle@yahoo.com>
2023-02-02 09:56:01 +0000

    CartMeshFilesGen.py changes - separating into different functions

--- src/CartMeshFilesGen.py
+++ src/CartMeshFilesGen.py
@@ -6,7 +6,10 @@ def mesh_files(x0, x1, y0, y1, z0, z1, nxCells, nyCells, nzCells):
 
     faces = get_faces(nxCells, nyCells, nzCells)
 
-    print(len(faces))
+
+
+    write_file(points, "MeshFiles/points.txt")
+    write_file(faces, "MeshFiles/faces.txt")
 
 def get_points(x0, x1, y0, y1, z0, z1, nxCells, nyCells, nzCells):
 
@@ -43,40 +46,61 @@ def get_points(x0, x1, y0, y1, z0, z1, nxCells, nyCells, nzCells):
 def get_faces(nxCells, nyCells, nzCells):
 
     faces = []
+    faces_back_front = []
+    faces_vertical = []
+    faces_horizontal = []
 
     for k in range(nzCells+1):
-        face_zlen = len(faces)
+        face_zlen = len(faces_back_front)
         for j in range(nyCells):
-            face_ylen = len(faces)
+            face_ylen = len(faces_back_front)
             for i in range(nxCells):
                     start_idx = i+face_ylen+face_zlen+j+k
                     face = [start_idx, start_idx+1, start_idx+1+nxCells, start_idx+2+nxCells]
+                    faces_back_front.append(face)
                     faces.append(face)
 
     for j in range(nyCells):
-        face_ylen = len(faces)
+        face_ylen = len(faces_vertical)
         for i in range(nxCells+1):
-                vertical_start = i+face_ylen
+                vertical_start = face_ylen+i
                 bottom_idx = (nxCells+1) * (nyCells+1)
                 face_vertical = [vertical_start, vertical_start+1+nxCells, vertical_start+bottom_idx, vertical_start+bottom_idx+nxCells+1]
-                faces.append(face_vertical)
+                faces_vertical.append(face_vertical)
+                if (i != 0) and (i != nxCells):
+                    faces.insert(0, face_vertical)
+                else:
+                    faces.append(face_vertical)
 
     for j in range(nyCells+1):
-        face_ylen = len(faces)
+        face_ylen = len(faces_horizontal)
         for i in range(nxCells):
                 horizontal_start = face_ylen+j+i
                 bottom_idx = (nxCells+1) * (nyCells+1)
                 face_horizontal = [horizontal_start, horizontal_start+1, horizontal_start+bottom_idx, horizontal_start+bottom_idx+1]
-                faces.append(face_horizontal)
+                faces_horizontal.append(face_horizontal)
+                if (j != 0) and (j != nyCells):
+                    faces.insert(0, face_horizontal)
+                else:
+                    faces.append(face_horizontal)
 
-    return np.array(faces) 
+    return np.array(faces)
 
-def write_points(points):
+def get_cells(faces):
 
-    with open("points.txt", "w") as file:
+    pass
+
+def write_file(points, filename):
+
+    with open(filename, "w") as file:
         for point in points:
-            point = f"({point[0]},{point[1]},{point[2]})\n"
-            file.writelines(point)
+            line = "("
+            for i in range(len(point)):
+                if (i == len(point)-1):
+                    line += f"{point[i]})\n"
+                else:
+                    line += f"{point[i]},"
+            file.writelines(line)
 
 if __name__ == "__main__":
 
