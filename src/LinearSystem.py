5c8c4f0b60483398f72d63a955b32c8f2074be64
Paddy Wardle <paddy.wardle@yahoo.com>
2023-02-08 15:02:00 +0000

    LinearSystem.py - updated discretisation

--- src/LinearSystem.py
+++ src/LinearSystem.py
@@ -4,12 +4,13 @@ import numpy as np
 
 class LinearSystem:
 
-    def __init__(self, mesh, viscosity):
+    def __init__(self, mesh, viscosity, alpha_u):
 
         self.mesh = mesh
-        self.viscoity = viscosity
+        self.viscosity = viscosity
+        self.alpha_u = alpha_u
 
-    def A_disc(self, u, dx):
+    def momentum_disc(self, u, uface, BCs):
 
         """
         This function discretises the momentum equation to get the diagonal and off-diagonal contributions to the linear system.
@@ -23,64 +24,72 @@ class LinearSystem:
         N = len(self.mesh.cells)
 
         A = np.zeros((N, N))
+        b = np.zeros((N, 1))
 
         for i in range(len(self.mesh.cells)):
 
-            diag_cont = 0
             neighbours = self.mesh.neighbouring_cells()[i]
             face_area_vectors = self.mesh.face_area_vectors()
 
-            # ADD LOGIC FOR THE FACE OWNER!
             cell_faces = self.mesh.cells[i]
+            centre_P = self.mesh.cell_centres()[i]
+
+            for face in self.mesh.cells[i]:
+
+                owner_neighbour = self.mesh.cell_owner_neighbour()[face]
+                sf = face_area_vectors[face]
+                face_mag = np.linalg.norm(sf)
+
+                if owner_neighbour[1] == -1 and sf[2] == 0:
+                    if sf[0] == 0:
+                        FN = face_mag * BCs[0]
+                    elif sf[1] == 0:
+                        FN = face_mag * BCs[1]
+
+                    A[i, i] += max(FN, 0) / self.alpha_u
+                    A[i, i] += -self.viscosity * face_mag / 0.05
 
             for j in neighbours:
 
-                off_diag = 0
-
+                # get faces in neighbour cell
                 neighbour_faces = self.mesh.cells[j]
+                #xprint(neighbour_faces)
+                # get the shared faces between the two cells
                 shared_face = list(set(cell_faces).intersection(neighbour_faces))[0]
+                # get the owner of the face
                 owner_neighbour = self.mesh.cell_owner_neighbour()[shared_face]
-
-                if owner_neighbour[0] != i:
+                #print(owner_neighbour)
+                # get centre of the neighbour cell
+                centre_N = self.mesh.cell_centres()[j]
+                # if cell is the owner of the face
+                if owner_neighbour[0] == i:
                     sf = face_area_vectors[shared_face]
                 else:
                     sf = -face_area_vectors[shared_face]
 
-                # second order postulation for face area velocity
-                u_face = u[i][i] + sf[0] * abs(u[i][i] - u[i][j])
+                face_mag = np.linalg.norm(sf)
 
-                # convective contribution
-                diag_cont += max(sf[1] * u_face, 0)
-                off_diag += min(sf[1] * u_face, 0)
+                FN = face_mag * uface[shared_face]
 
-                # diffusion contributions
-                diag_cont += -self.viscoity * abs(sf[1]) / dx#sf[1] <- FIX THIS
-                off_diag += self.viscoity * abs(sf[1]) / dx#sf[1] <- FIX THIS
+                d = abs(centre_P - centre_N)
+                d_mag = np.linalg.norm(d)
+                # calculate face flux
 
-                A[i, j] = off_diag
-            
-            A[i, i] = diag_cont
+                # convection contributions
+                A[i, i] += max(FN, 0)
+                A[i, j] += min(FN, 0)
 
-        #A_sparse = SparseMatrixCR(N, N).from_dense(A)
+                # diffusive contributions
+                A[i, i] += -self.viscosity * face_mag / d_mag
+                A[i, j] += self.viscosity * face_mag / d_mag
 
-        return A
+        for i in range(len(A)):
+            A[i, i] /= self.alpha_u
+            b[i] = ((1-self.alpha_u)/self.alpha_u) * u[i] * A[i, i]
 
-    def b_disc(self):
-
-        """
-        This function discretises the momentum equation to get the source contributions to the linear system.
-
-        Returns:
-            np.array: N x 1 matrix defining the source contributions to the linear system.
-
-        """
-
-        # steady state simulation so will be a vector of zeros
-        N = len(self.mesh.cells)
-
-        return np.zeros((N, 1)) 
-
-    def gauss_seidel(self, A, b, tol=1e-6, maxIts=1000):
+        return A, b
+    
+    def gauss_seidel(self, A, b, u, start_it=False, tol=1e-6, maxIts=1000):
 
         """
         This function uses the Gauss-Seidel algorithm to solve the linear system.
@@ -97,11 +106,12 @@ class LinearSystem:
 
         it = 0
         # set initial guess for x and initial residual value
-        x_initial = np.ones(b.shape)
+        x_initial = np.reshape(u, b.shape)
         x = x_initial
-        res_initial = np.abs(b - np.matmul(A, x_initial)).sum() # Udine and Jasak <- ADD REFERENCE
-        res = np.abs(b - np.matmul(A, x)).sum() / res_initial
-        
+        x_plus1 = x_initial
+        res_initial = np.linalg.norm(b - np.matmul(A, x_initial)) + 1e-4 # Udine and Jasak <- ADD REFERENCE
+        res = res_initial / res_initial
+
         # while number iterations is less than 
         while (it < maxIts) and (res > tol):
 
@@ -109,7 +119,7 @@ class LinearSystem:
             strictly_upper_tri = np.triu(A, 1)
             x_plus1 = np.matmul(np.linalg.inv(lower_tri), (b - np.matmul(strictly_upper_tri, x)))
             x = x_plus1
-            res = np.abs(b - np.matmul(A, x)).sum() / res_initial # Udine and Jasak <- ADD REFERENCE
+            res = np.linalg.norm(b - np.matmul(A, x)) / res_initial # Udine and Jasak <- ADD REFERENCE
             it += 1
         
         print(f"Gauss-Seidel Final Iterations = {it}")
