9655175d5ff07d81e913c12e478c26937884cceb
Paddy Wardle <paddy.wardle@yahoo.com>
2023-02-09 18:16:20 +0000

    InitialConds.py initial commit

---
+++ src/utils/InitialConds.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+def InitialConds():
+
+    with open("MeshFiles/cells_test.txt") as f:
+        num_cells = len(f.readlines())
+
+    u_field = np.zeros((num_cells, 1))
+    v_field = np.zeros((num_cells, 1))
+    p_field = np.zeros((num_cells, 1))
+    
+    WriteFile("InitialConds/u_field.txt", u_field)
+    WriteFile("InitialConds/v_field.txt", v_field)
+    WriteFile("InitialConds/p_field.txt", p_field)
+
+def WriteFile(filename, data):
+
+    with open(filename, "w") as f:
+        for i in data:
+            f.write(str(i[0]))
+            f.write("\n")
+
+def ReadFile(filename):
+
+    data = []
+
+    with open(filename) as f:
+        for line in f.readlines():
+            data.append(line)
+    
+    return np.array(data)
+
+if __name__ == "__main__":
+
+    InitialConds()
