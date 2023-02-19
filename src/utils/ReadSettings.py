78eda0c4325b9aa2fbd84ad3d367116fea99f69d
Paddy Wardle <paddy.wardle@yahoo.com>
2023-02-19 19:31:14 +0000

    ReadSettings - updating to calculate length

--- src/utils/ReadSettings.py
+++ src/utils/ReadSettings.py
@@ -8,11 +8,13 @@ def ReadJSON(filename):
 def ReadSettings(filename):
 
     simulation_sets = ReadJSON(filename)['SIMULATION']
+    MESH_sets = ReadJSON(filename)['MESH']
     Re = simulation_sets['Re']
     alpha_u = simulation_sets['alpha_u']
     alpha_p = simulation_sets['alpha_p']
     SIMPLE_tol = simulation_sets['SIMPLE_tol']
     tol_GS = simulation_sets['Gauss-Seidel']['tol']
     maxIts = simulation_sets['Gauss-Seidel']['maxIts']
+    L = float(MESH_sets['x1']) - float(MESH_sets['x0'])
     
-    return Re, alpha_u, alpha_p, SIMPLE_tol, tol_GS, maxIts
+    return Re, alpha_u, alpha_p, SIMPLE_tol, tol_GS, maxIts, L
