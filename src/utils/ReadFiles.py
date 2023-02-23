36cb7689a0e42c2b54e7a0d32f94b964f0507a3d
Paddy Wardle <paddy.wardle@yahoo.com>
2023-02-23 20:25:33 +0000

    ReadFiles.py initial commit

---
+++ src/utils/ReadFiles.py
@@ -0,0 +1,105 @@
+import json
+import numpy as np
+
+class ReadFiles():
+
+    """
+    Class to read files needed for the simulation
+    """
+
+    def __init__(self):
+
+        pass
+
+    def ReadJSON(self, filename):
+
+        """
+        Function to read JSON files
+
+        Args:
+            filename (string): name of json file
+        """
+
+        with open(filename, 'r') as f:
+            return json.load(f)
+
+    def ReadSettings(self, filename):
+
+        """
+        Function to read settings files
+
+        Args:
+            filename (string): settings file name
+        """
+
+        simulation_sets = self.ReadJSON(filename)['SIMULATION']
+        MESH_sets = self.ReadJSON(filename)['MESH']
+        Re = simulation_sets['Re']
+        alpha_u = simulation_sets['alpha_u']
+        alpha_p = simulation_sets['alpha_p']
+        SIMPLE_tol = simulation_sets["SIMPLE"]['tol']
+        SIMPLE_its = simulation_sets["SIMPLE"]["its"]
+        tol_GS = simulation_sets['Gauss-Seidel']['tol']
+        maxIts = simulation_sets['Gauss-Seidel']['maxIts']
+        L = float(MESH_sets['x1']) - float(MESH_sets['x0'])
+        
+        return Re, alpha_u, alpha_p, SIMPLE_tol, SIMPLE_its, tol_GS, maxIts, L
+
+    def ReadMesh(self, points_filename, faces_filename, cells_filename, boundary_filename):
+
+        """
+        Function to read different mesh files
+
+        Args:
+            points_filename (string): filename of the points file
+            faces_filename (string): filename of the string file
+            cells_filename (string): filename of the cells file
+            boundary_filename (string): filename of the boundary patches file
+        """
+
+        # call read_file function for each mesh characteristic and return arrays
+        points = np.asarray(self.ReadMeshFile(points_filename))
+        faces = np.asarray(self.ReadMeshFile(faces_filename), dtype=int)
+        cells = np.asarray(self.ReadMeshFile(cells_filename), dtype=int)
+        boundary = np.asarray(self.ReadMeshFile(boundary_filename), dtype=object)
+
+        return points, faces, cells, boundary
+
+    def ReadMeshFile(self, filename):
+
+        """
+        Function to read mesh single file
+
+        Args:
+            filename (string): name of file to be read
+        """
+
+        array = []
+
+        # read each file for the mesh, appropriately format and return array of mesh characteristic
+        with open("MeshFiles/" + filename, 'r') as f:
+            for line in f.readlines():
+                line = line.strip()
+                ls = line.strip('()').split()
+                if filename != "boundary_patches2.txt":
+                    ls = [float(i) for i in ls]
+                array.append(ls)
+
+        return array
+    
+    def ReadInitialConds(self, filename):
+
+        """
+        Function to read single initial field file
+
+        Args:
+            filename (string): name of field file
+        """
+
+        data = []
+
+        with open(filename) as f:
+            for line in f.readlines():
+                data.append(float(line.strip()))
+        
+        return np.array(data, dtype=float)
