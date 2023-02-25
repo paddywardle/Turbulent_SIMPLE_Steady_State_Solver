31c3dcfae482c4e24e225b41cf397ecac0dbd86d
Paddy Wardle <paddy.wardle@yahoo.com>
2023-02-25 16:00:09 +0000

    WriteFiles updated with creating directory and SIM_num

--- src/utils/WriteFiles.py
+++ src/utils/WriteFiles.py
@@ -1,12 +1,14 @@
+import os
+
 class WriteFiles():
 
     """
     Class to write results from simulation
     """
 
-    def __init__(self):
+    def __init__(self, SIM_num):
 
-        pass
+        self.SIM_num = SIM_num
 
     def WriteFile(self, filename, field):
 
@@ -18,11 +20,11 @@ class WriteFiles():
             field (np.array): field to write to file
         """
     
-        with open("Results/"+filename+".txt", "w") as f:
+        with open(f"Results/SIM {self.SIM_num}/"+filename+".txt", "w") as f:
             for i in range(len(field)):
                 f.write(str(field[i]) + "\n")
 
-    def WriteResults(self, u_field, v_field, p_field, SIMPLE_residuals):
+    def WriteResults(self, u_field, v_field, p_field, res_SIMPLE_ls, resx_momentum_ls, resy_momentum_ls, res_pressure):
 
         """
         Function to write out the velocity fields, pressure field and residuals
@@ -33,8 +35,13 @@ class WriteFiles():
             p_field (np.array): pressure field
             SIMPLE_residuals (np.array): SIMPLE residuals
         """
+        if not os.path.exists(f"Results/SIM {self.SIM_num}"):
+            os.mkdir(f"Results/SIM {self.SIM_num}")
 
         self.WriteFile("u_field", u_field)
         self.WriteFile("v_field", v_field)
         self.WriteFile("p_field", p_field)
-        self.WriteFile("residuals", SIMPLE_residuals)
+        self.WriteFile("res_SIMPLE", res_SIMPLE_ls)
+        self.WriteFile("resx_momentum", resx_momentum_ls)
+        self.WriteFile("resy_momentum", resy_momentum_ls)
+        self.WriteFile("res_pressure", res_pressure)
