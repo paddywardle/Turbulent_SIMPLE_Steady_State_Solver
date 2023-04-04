a7371eef16a454fc5bbaa906d8050d0eff0cbace
Paddy Wardle <paddy.wardle@yahoo.com>
2023-04-04 16:26:46 +0100

    InitialConds updated - adding k and e fields

--- src/utils/InitialConds.py
+++ src/utils/InitialConds.py
@@ -8,10 +8,14 @@ def InitialConds():
     u_field = np.zeros((num_cells, 1))
     v_field = np.zeros((num_cells, 1))
     p_field = np.zeros((num_cells, 1))
+    k_field = np.zeros((num_cells, 1))
+    e_field = np.zeros((num_cells, 1))
     
     WriteFile("InitialConds/20x20/u_field.txt", u_field)
     WriteFile("InitialConds/20x20/v_field.txt", v_field)
     WriteFile("InitialConds/20x20/p_field.txt", p_field)
+    WriteFile("InitialConds/20x20/k_field.txt", k_field)
+    WriteFile("InitialConds/20x20/e_field.txt", e_field)
 
 def WriteFile(filename, data):
 
