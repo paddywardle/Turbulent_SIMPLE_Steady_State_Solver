1e7d8b77058d6a28996c7ddf62ee426221812b2c
Paddy Wardle <paddy.wardle@yahoo.com>
2023-04-06 17:52:52 +0100

    InitialConds update for e initial conditions

--- src/utils/InitialConds.py
+++ src/utils/InitialConds.py
@@ -1,6 +1,7 @@
 import numpy as np
 from ReadFiles import ReadFiles
 
+
 def InitialConds():
 
     read = ReadFiles()
@@ -13,7 +14,7 @@ def InitialConds():
     v_field = np.zeros((num_cells, 1))
     p_field = np.zeros((num_cells, 1))
     k_field = np.ones((num_cells, 1))
-    e_field = np.ones((num_cells, 1))
+    e_field = ((Cmu ** 0.75) * np.power(k_field, 1.5))/0.1
     
     WriteFile("InitialConds/20x20/u_field.txt", u_field)
     WriteFile("InitialConds/20x20/v_field.txt", v_field)
