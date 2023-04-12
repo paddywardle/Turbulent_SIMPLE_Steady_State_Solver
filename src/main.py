5842e609ca3d0274f9936f559f6588dc01f52248
Paddy Wardle <paddy.wardle@yahoo.com>
2023-04-12 13:05:46 +0100

    main.py - altering for new mesh

--- src/main.py
+++ src/main.py
@@ -32,18 +32,9 @@ if __name__ == "__main__":
     # calculate kinematic viscosity
     viscosity = L/Re
 
-    if directory == "20x20":
-        dim = 20
-    elif directory == "40x40":
-        dim = 40
-    elif directory == "100x100":
-        dim = 100
+    dim = 10
 
-    # read in mesh and initialise mesh class using data
-    points, faces, cells, owners, neighbours, boundary = read.ReadMesh(directory+"/points.txt", directory+"/faces.txt", directory+"/cells.txt", 
-                                                   directory+"/owner.txt", directory+"/neighbour.txt", directory+"/boundary_patches.txt")
-
-    mesh = Mesh(points, faces, cells, owners, neighbours, boundary)
+    mesh = Mesh(f"MeshFiles/{directory}")
 
     # set initial conditions for the simulation (Ux, Uy, and P) <- assuming fluid is at rest at the start of the simulation
     u_field = read.ReadInitialConds("InitialConds/"+directory+"/u_field.txt")
@@ -57,10 +48,10 @@ if __name__ == "__main__":
 
     simple = SIMPLE(mesh, conv_scheme, viscosity, alpha_u, alpha_p, Cmu, C1, C2, C3, sigmak, sigmaEps)
 
-    u, v, z, p, k, e, res_SIMPLE_ls, resx_momentum_ls, resy_momentum_ls, res_pressure, mat_coeffs, iterations = simple.iterate(u_field, v_field, p_field, k_field, e_field, dim, SIMPLE_tol, SIMPLE_its)
+    u, v, z, p, k, e, res_SIMPLE_ls, resx_momentum_ls, resy_momentum_ls, res_pressure, iterations = simple.iterate(u_field, v_field, p_field, k_field, e_field, dim, SIMPLE_tol, SIMPLE_its)
 
     simulation_run_time = round(time.perf_counter() - start_time, 2)
 
     print("Simulation run time: " + str(simulation_run_time))
 
-    write.WriteResults(u, v, z, p, k, e, res_SIMPLE_ls, resx_momentum_ls, resy_momentum_ls, res_pressure, simulation_run_time, mat_coeffs, directory, iterations)
+    write.WriteResults(u, v, z, p, k, e, res_SIMPLE_ls, resx_momentum_ls, resy_momentum_ls, res_pressure, simulation_run_time, directory, iterations)
