c727401ba902c32d1a782901738fc75fb66fdabb
Paddy Wardle <paddy.wardle@yahoo.com>
2023-04-12 13:04:23 +0100

    InitialConds - updating for backward step

--- src/utils/InitialConds.py
+++ src/utils/InitialConds.py
@@ -1,14 +1,15 @@
+import sys
 import numpy as np
 from ReadFiles import ReadFiles
-
+from MeshParser import MeshParser
 
 def InitialConds():
 
     read = ReadFiles()
     Re, alpha_u, alpha_p, conv_scheme, SIMPLE_tol, SIMPLE_its, GS_tol, maxIts, L, directory, Cmu, C1, C2, C3, sigmak, sigmaEps = read.ReadSettings('config/config.json')
 
-    with open("MeshFiles/20x20/cells.txt") as f:
-        num_cells = len(f.readlines())
+    mesh = MeshParser(f"MeshFiles/{directory}")
+    num_cells = len(mesh.cells)
 
     u_field = np.zeros((num_cells, 1))
     v_field = np.zeros((num_cells, 1))
@@ -16,11 +17,11 @@ def InitialConds():
     k_field = np.ones((num_cells, 1))
     e_field = ((Cmu ** 0.75) * np.power(k_field, 1.5))/0.1
     
-    WriteFile("InitialConds/20x20/u_field.txt", u_field)
-    WriteFile("InitialConds/20x20/v_field.txt", v_field)
-    WriteFile("InitialConds/20x20/p_field.txt", p_field)
-    WriteFile("InitialConds/20x20/k_field.txt", k_field)
-    WriteFile("InitialConds/20x20/e_field.txt", e_field)
+    WriteFile(f"InitialConds/{directory}/u_field.txt", u_field)
+    WriteFile(f"InitialConds/{directory}/v_field.txt", v_field)
+    WriteFile(f"InitialConds/{directory}/p_field.txt", p_field)
+    WriteFile(f"InitialConds/{directory}/k_field.txt", k_field)
+    WriteFile(f"InitialConds/{directory}/e_field.txt", e_field)
 
 def WriteFile(filename, data):
 
