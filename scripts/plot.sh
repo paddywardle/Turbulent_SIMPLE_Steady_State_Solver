dc43f2a3960dab39861faf6887204d3e49b003a3
Paddy Wardle <paddy.wardle@yahoo.com>
2023-04-22 10:37:36 +0100

    Removing unneccessary scripts

--- scripts/plot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python -OO ./plots/plot_script.py --SIM_num $1
--- scripts/run_and_plot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python -OO ./src/main.py --SIM_num $1
-python -OO ./plots/plot_script.py --SIM_num $1
-cp config/config.json Results/"SIM $1"/config.json
