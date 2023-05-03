fa020829422d46215b738f606e51b5e9d7b9ccf1
Paddy Wardle <paddy.wardle@yahoo.com>
2023-05-03 16:12:41 +0100

    utils updates

--- src/utils/InitialConds.py
+++ src/utils/InitialConds.py
@@ -16,8 +16,8 @@ def InitialConds():
     w_field = np.zeros((num_cells, 1))
     U = np.hstack((u_field, v_field, w_field))
     p_field = np.zeros((num_cells, 1))
-    k_field = np.zeros((num_cells, 1)) * 0.375
-    e_field = np.zeros((num_cells, 1)) * 14.855
+    k_field = np.ones((num_cells, 1)) * 0.00135
+    e_field = np.ones((num_cells, 1)) * 0.003
     #k_field = (3/2) * np.square(BC[0]['inlet'][4] * u_field)
     #e_field = ((Cmu ** 0.75) * np.power(k_field, 1.5))/0.1
     
