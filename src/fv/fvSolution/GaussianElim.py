579dd2d32ec69d2c3189369bb54d1e9868abad86
Paddy Wardle <paddy.wardle@yahoo.com>
2023-05-30 11:37:40 +0100

    GaussianElim update

--- src/fv/fvSolution/GaussianElim.py
+++ src/fv/fvSolution/GaussianElim.py
@@ -27,13 +27,3 @@ class GaussianElimination():
                     A[i,j] -= A[i,k] * A[k,j]
 
         return A
-
-if __name__ == "__main__":
-
-    A = np.array([[2, 2, 3], [5, 9, 10], [4, 1, 2]])
-
-    GE = GaussianElimination()
-
-    A = GE.ge(A)
-
-    print(A)
